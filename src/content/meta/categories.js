d0ee4dd50f041d5b8ff0460502ea4b4578605cb3
nik <nikhil.lal@gmail.com>
2020-05-21 15:30:50 -0400

    revised isee arm and added engine category

--- src/content/meta/categories.js
+++ src/content/meta/categories.js
@@ -5,6 +5,7 @@ import FolderPlusIcon from 'react-feather/dist/icons/folder-plus';
 
 export const categories = [
   { name: 'projects', label: 'Personal Projects', icon: ImageIcon },
+  { name: 'engine', label: 'The Engine', icon: FolderPlusIcon }
   { name: 'biobot', label: 'Biobot Analytics', icon: FolderPlusIcon },
   { name: 'sensata', label: 'Sensata Technologies', icon: FolderPlusIcon },
   { name: 'cornell', label: 'Cornell University', icon: FolderPlusIcon }
